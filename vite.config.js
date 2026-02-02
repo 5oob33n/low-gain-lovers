c325c77d24f58772183cff6ac1cc7c4d20c53f79
Soobeen Woo <swoo@uk-video-mbp.local>
2026-02-02 23:28:29 +0100

    Build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/low-gain-lovers/',
   plugins: [react()],
-  build: {
-    outDir: 'docs'
-  }
+  build: { outDir: 'docs' },
 })
